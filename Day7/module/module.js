60e318440673ecb2d146e422acbd15d4a863eb6d
Tôn Hội <tonthathoi.090302@gmail.com>
2022-03-30 00:52:39 +0700

    day 7

---
+++ Day7/module/module.js
@@ -0,0 +1,15 @@
+// có rất nhiều cách để biểu diễn 1 file dưới dạng module
+// cách 1
+// export default function logger() {
+//   console.log("123");
+// }
+
+// cách 2
+// const logger = (e) => {
+//   console.log("123");
+// };
+
+// export default logger;
+
+// cách 3
+export const text = "123";
---
+++ Day7/strict_mode.js
@@ -0,0 +1,6 @@
+//  đây là một chế độ nghiêm ngặt trong javascript
+
+"use strict";
+
+a = "text";
+console.log(a); // đoạn code này sẽ được báo lỗi khi nằm trong chế độ strict mode
